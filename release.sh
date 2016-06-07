adc4ee159961af88de23083038d5555746dc23dd
Sergey Mironov <grrwlf@gmail.com>
2016-06-07 17:13:31 +0000

    Add release.sh

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+oops() {
+  echo "$@" >&2
+  exit 1
+}
+
+set -x
+while true ; do
+
+  master=`git branch -v | grep master | tr -d '*' | awk '{print $2}'`
+  release=`git branch -v | grep release | tr -d '*' | awk '{print $2}'`
+
+  if test "$release" != "$master" ; then
+    git merge --no-ff master || oops "merge failed"
+    cabal build
+  fi
+
+  sleep 5
+
+done
